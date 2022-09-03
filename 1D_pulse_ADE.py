387edb4cd0e1f96f1af2f5d456f82534879ef378
LBorsky <96687255+LBorsky@users.noreply.github.com>
2022-09-03 16:32:22 +0300

    Update 1D_pulse_ADE.py

--- 1D_pulse_ADE.py
+++ 1D_pulse_ADE.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def conc(x,t=1,Q=5,D=0.005,x_0=0,U=1,sig=3):
+def conc(x,T=1,Q=5,D=0.005,x_0=0,U=1,sig=3):
     return Q / (4 * np.pi * D * t) ** 0.5 * np.exp(-((x - x_0 - U * t) ** 2) / (2 * sig ** 2))
     
 xlist = np.linspace(0,100,num=1000)
@@ -12,8 +12,8 @@ xlist = np.linspace(0,100,num=1000)
 # streamlit
 st.title("1-D ADE solution, IC=pulse")
 t = st.slider("Time of sampling [sec]", 1, 50, 5)
-U = st.slider("Velocity of fluid [m/sec]", 0.1, 5, 1)
-ylist = conc(x=xlist,t=t,U=U)
+u = st.slider("Velocity of fluid [m/sec]", 0.1, 5, 1)
+ylist = conc(x=xlist,T=t,U=u)
 fig, ax = plt.subplots(figsize=(6,6))
 plt.axis([0,50,0,50])
 ax.plot(xlist, ylist, color="blue", linewidth=2)
