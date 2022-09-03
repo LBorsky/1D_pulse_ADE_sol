2fab3e51bc2e2276e49f7c5d9db9326fbfa25484
LBorsky <96687255+LBorsky@users.noreply.github.com>
2022-09-03 15:54:59 +0300

    Update 1D_pulse_ADE.py

--- 1D_pulse_ADE.py
+++ 1D_pulse_ADE.py
@@ -1,16 +1,20 @@
+# function
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-ax = plt.axes(projection="3d")
+def conc(x,t=1,Q=5,D=0.001,x_0=0,U=1,sig=3):
+    return Q / (4 * np.pi * D * t) ** 0.5 * np.exp(-((x - x_0 - U * t) ** 2) / (2 * sig ** 2))
+    
+xlist = np.linspace(0,100,num=1000)
 
-def z_function(x, y):
-    return np.sin(np.sqrt(x ** 2 + y ** 2))
+# streamlit
 
-x = np.linspace(-5, 5, 100)
-y = np.linspace(-5, 5, 100)
-
-X, Y = np.meshgrid(x, y)
-Z = z_function(X, Y)
-
-ax.plot_surface(X, Y, Z)
-plt.show()
+st.title("1-D ADE solution, IC=pulse")
+t = st.slider("Time of sampling [s]", 0.1, 50.1, 5)
+ylist = conc(x=xlist,t=t)
+fig, ax = plt.subplots(figsize=(6,6))
+ax.plot(xlist, ylist)
+ax.set_xlabel('$t$ (sec)')
+ax.set_ylabel('$C$ (mg/L)')
+st.pyplot(fig)
