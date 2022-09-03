6c7c87b01d89c7f29e33606d3a7e9abfc22592d2
LBorsky <96687255+LBorsky@users.noreply.github.com>
2022-09-03 18:23:29 +0300

    Update 1D_pulse_ADE.py

--- 1D_pulse_ADE.py
+++ 1D_pulse_ADE.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 
-def conc(x,T=1,Q=5,D=0.005,X_0=0,U=1,SIG=3):
-    return Q / (4 * np.pi * D * T) ** 0.5 * np.exp(-((x - X_0 - U * T) ** 2) / (2 * SIG ** 2))
+def conc(x,T=1,Q=5,D=0.005,X_0=0,U=1,S=3):
+    return Q / (4 * np.pi * D * T) ** 0.5 * np.exp(-((x - X_0 - U * T) ** 2) / (2 * S ** 2))
     
 xlist = np.linspace(0,100,num=1000)
 
@@ -14,8 +14,8 @@ t = st.slider("Time of sampling [sec]", 1, 50, 5)
 q = st.slider("Initial mass/area of contaminant [kg/m^2]", 1, 10, 5)
 u = st.slider("Velocity of fluid [m/sec]", 0, 5, 2)
 x_0 = st.slider("Location of contemination [m]", 0, 40, 0)
-SIG = st.slider("STD [m]", 0, 10, 3)
-ylist = conc(x=xlist,T=t,Q=q,X_0=x_0,U=u,SIG=sig)
+s = st.slider("STD [m]", 0, 10, 3)
+ylist = conc(x=xlist,T=t,Q=q,X_0=x_0,U=u,S=s)
 fig, ax = plt.subplots(figsize=(6,6))
 plt.axis([0,100,0,50])
 ax.plot(xlist, ylist, color="blue", linewidth=2)
