331c778782aac71c1681ddf0587a11706b97baba
LBorsky <96687255+LBorsky@users.noreply.github.com>
2022-09-03 16:00:01 +0300

    Update 1D_pulse_ADE.py

--- 1D_pulse_ADE.py
+++ 1D_pulse_ADE.py
@@ -3,7 +3,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
+st.title("1-D ADE solution, IC=pulse")
 
+"""
 def conc(x,t=1,Q=5,D=0.001,x_0=0,U=1,sig=3):
     return Q / (4 * np.pi * D * t) ** 0.5 * np.exp(-((x - x_0 - U * t) ** 2) / (2 * sig ** 2))
     
